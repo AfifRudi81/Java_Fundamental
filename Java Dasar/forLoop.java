35ff73d7ce3420cc2a3fd8c4e3acc458353c504d
AfifRudi81 <afif.nf81@gmail.com>
2025-03-31 22:31:22 +0700

    lanjut belajar java episode-47

--- Java Dasar/forLoop.java
+++
@@ -1,7 +0,0 @@
-public class forLoop {
-    public static void main(String[] args) {
-        for(var i=1;i<=10;i++){
-            System.out.println("Kamu berhasil "+ i);
-        }
-    }
-}
--- Java Dasar/whileLoop.java
+++
@@ -1,9 +0,0 @@
-public class whileLoop {
-    public static void main(String[] args) {
-        var num = 2;
-        while(num <= 5){
-            System.out.println("kamu berhasil,lanjut kesesi selanjutnya " + num);
-            num++;
-        }
-    }
-}
